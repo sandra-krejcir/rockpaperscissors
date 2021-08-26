884a6ad6c0cd1e2e5179647ca1740ee0c00c2297
Sandra Krejcir <krejcir.sandra@gmail.com>
2021-08-26 21:18:25 +0200

    restart as a reload+fixed the shake thingy

--- script.js
+++ script.js
@@ -46,56 +46,66 @@ function showAnimation() {
 
 function revealWinner() {
   console.log("function revealWinner()");
-  if (choiceUser === "rock") {
-    document.querySelector("#player2").classList.add("rock");
-    if (compChoice === 0) {
-      document.querySelector("#player1").classList.add("scissors");
+  document.querySelector("#player1").classList.remove("shake");
+  document.querySelector("#player2").classList.remove("shake");
+  if (compChoice === 0) {
+    document.querySelector("#player1").classList.add("scissors");
+    if (choiceUser === "rock") {
+      document.querySelector("#player2").classList.add("rock");
       youWin();
-    } else if (compChoice === 1) {
-      document.querySelector("#player1").classList.add("paper");
+    } else if (choiceUser === "paper") {
+      document.querySelector("#player2").classList.add("paper");
       youLose();
     } else {
-      document.querySelector("#player1").classList.add("rock");
+      document.querySelector("#player2").classList.add("scissors");
       draw();
     }
-  } else if (choiceUser === "paper") {
-    document.querySelector("#player2").classList.add("paper");
-    if (compChoice === 0) {
-      document.querySelector("#player1").classList.add("scissors");
+  } else if (compChoice === 1) {
+    document.querySelector("#player1").classList.add("paper");
+    if (choiceUser === "rock") {
+      document.querySelector("#player2").classList.add("rock");
       youLose();
-    } else if (compChoice === 1) {
-      document.querySelector("#player1").classList.add("paper");
+    } else if (choiceUser === "paper") {
+      document.querySelector("#player2").classList.add("paper");
       draw();
     } else {
-      document.querySelector("#player1").classList.add("rock");
+      document.querySelector("#player2").classList.add("scissors");
       youWin();
     }
   } else {
-    document.querySelector("#player2").classList.add("scissors");
-    if (compChoice === 0) {
-      document.querySelector("#player1").classList.add("scissors");
+    document.querySelector("#player1").classList.add("rock");
+    if (choiceUser === "rock") {
+      document.querySelector("#player2").classList.add("rock");
       draw();
-    } else if (compChoice === 1) {
-      document.querySelector("#player1").classList.add("paper");
+    } else if (choiceUser === "paper") {
+      document.querySelector("#player2").classList.add("paper");
       youWin();
     } else {
-      document.querySelector("#player1").classList.add("rock");
+      document.querySelector("#player2").classList.add("scissors");
       youLose();
     }
   }
+  //0-scissors, 1-paper, 2-rock
 }
 
 function youWin() {
   console.log("function youWin()");
   document.querySelector("#win").classList.remove("hidden");
+  window.setInterval("restart()", 2000);
 }
 
 function youLose() {
   console.log("function youLose()");
   document.querySelector("#lose").classList.remove("hidden");
+  window.setInterval("restart()", 2000);
 }
 
 function draw() {
   console.log("function draw()");
   document.querySelector("#draw").classList.remove("hidden");
+  window.setInterval("restart()", 2000);
+}
+
+function restart() {
+  window.location.reload();
 }
